12822128f6e0b6fd009e1f13ac9d48f96840949e
dcarrsf <dan@dancarr.co>
2017-06-15 20:53:01 -0700

    Adding Webpack dev configuration.

---
+++ webpack/config.dev.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // Start at the context path
+  context: path.resolve(__dirname, '../src'),
+  // Entry point...
+  entry: {
+    app: './main.jsx',
+  },
+  // Output endpoint (development)
+  output: {
+    path: '/',
+    filename: '[name].bundle.js',
+    publicPath: 'http://localhost:8080/',
+  },
+  // Add '.jsx' to the resolve.extensions array.
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  // Run the Dev Server (Hot reloading)
+  devServer: {
+    contentBase: path.resolve(__dirname, '../src'),
+  },
+  // Run tasks with loaders...
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: [
+          'babel-loader',
+        ],
+      },{
+        test: /\.(sass|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'resolve-url-loader', 
+          'sass-loader?sourceMap'
+        ]
+      },{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        enforce: 'pre',
+        use: [{loader: 'eslint-loader', options: {rules: {semi: 0}}}],
+      },
+    ],
+  },
+  plugins: [
+    // Avoid publishing files when compilation fails
+    new webpack.NoEmitOnErrorsPlugin(),
+    // Generate the index.html file
+    new HtmlWebpackPlugin({
+      title: 'Disney Code Challenge',
+      template: '../src/main.ejs',
+      filename: '../index.html'
+    })
+  ],
+};
